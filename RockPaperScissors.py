0c8d2f5b869e74a4d5d8967bec52f75fe2e47b93
Priyanshu Maurya <182461045+PriyanshuNainsi@users.noreply.github.com>
2025-01-23 18:13:20 +0530

    RockPaper
    RockPaper.

---
+++ RockPaperScissors.py
@@ -0,0 +1,18 @@
+import random
+opt=['Rock','Paper','Scissors']
+a=input("Enter you option:")
+c=random.choice(opt)
+print(c)
+
+if(a==c):
+    print("Tie")
+
+elif(a=='Scissors' and c=='Rock'):
+    print("you won")
+elif(a=='Paper' and c=='Rock'):
+    print("You Won")
+elif(a=='Rock' and c=='Paper'):
+    print("YOu won ")
+
+else:print("computer won")
+
